719a35e71e610187bb48e1983b09382ec1905537
Jax <13301132691@163.com>
2017-06-06 23:07:54 +0800

    quicksort

---
+++ quicksort.py
@@ -0,0 +1,11 @@
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+
+print(quicksort([1, 3, 6, 5, 7, 3, 7, 9, 0]))
